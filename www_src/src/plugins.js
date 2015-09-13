e199153c505dca33378d854833499adc9346cb1f
Simon <wq@zlzkj.com>
2015-09-14 00:16:42 +0800

    update: ble send data to HC-08蓝牙模块；

--- www_src/src/plugins.js
+++ www_src/src/plugins.js
@@ -1,13 +1,13 @@
 define(function() {
 
-	/**
-	 * 调用原生activity插件                [description]
-	 */
+    /**
+     * 调用原生activity插件                [description]
+     */
     function startActivity(activityClassName,jsonData,successCallback) {
         if (window.cordova && window.cordova.plugins.activity) {
             window.cordova.plugins.activity.start(activityClassName,jsonData,successCallback);
         } else {
-        	console.log("未找到activity插件");
+            console.log("未找到activity插件");
         }
     }
 
@@ -25,29 +25,89 @@ define(function() {
             alert("未找到ble插件");
         }
     }
-    function bleStartScan(services, success, failure) {
-        bleCheck();
-        window.ble.startScan(services, success, failure);
+    function bleOnError(errorMessage) {
+        alert("ERROR: " + errorMessage);
     }
-    function bleStopScan(success, failure) {
+    function bleStartScan(services, success) {
         bleCheck();
-        window.ble.stopScan(success, failure);
+        window.ble.startScan(services, success, bleOnError);
     }
-    function bleConnect(device_id, connectSuccess, connectFailure) {
+    function bleStopScan(success) {
         bleCheck();
-        window.ble.connect(device_id, connectSuccess, connectFailure);
+        window.ble.stopScan(success, bleOnError);
     }
-    function bleDisconnect(device_id, disConnectSuccess, disConnectFailure) {
+    function bleConnect(device_id, success) {
         bleCheck();
-		window.ble.disconnect(device_id, disConnectSuccess, disConnectFailure);
+        window.ble.connect(device_id, success, bleOnError);
+        // success中返回peripheral对象，类似下列结构
+        // {
+        //     "characteristics": [{
+        //         "descriptors": [{
+        //             "uuid": "00002902-0000-1000-8000-00805f9b34fb"
+        //         }, {
+        //             "uuid": "00002901-0000-1000-8000-00805f9b34fb"
+        //         }],
+        //         "characteristic": "0000ffe1-0000-1000-8000-00805f9b34fb",
+        //         "service": "0000ffe0-0000-1000-8000-00805f9b34fb",
+        //         "properties": ["Read", "WriteWithoutResponse", "Notify"]
+        //     }],
+        //     "advertising": {},
+        //     "id": "20:C3:8F:FD:6D:76",
+        //     "services": ["0000ffe0-0000-1000-8000-00805f9b34fb"],
+        //     "rssi": -46,
+        //     "name": "LANYA"
+        // }
+    }
+    function bleDisconnect(device_id, success) {
+        bleCheck();
+        window.ble.disconnect(device_id, success, bleOnError);
+    }
+    function bleWrite(peripheral, data, success, error) {
+        bleCheck();
+        var characteristic = getCharacteristicByMode(peripheral.characteristics);
+        if (characteristic) {
+            window.ble.writeWithoutResponse(peripheral.id, characteristic.service, characteristic.characteristic, data, success, error);
+        } else {
+            alert("该蓝牙模块不是HC-08模块，无法读写");
+            error();
+        }
+    }
+    function getCharacteristicByMode(characteristics) {
+        for (var i in characteristics) {
+            //蓝牙4.0的UUID,其中0000ffe1-0000-1000-8000-00805f9b34fb是广州汇承信息科技有限公司08蓝牙模块的UUID
+            if (characteristics[i].characteristic == "0000ffe1-0000-1000-8000-00805f9b34fb") {
+                return characteristics[i];
+            } 
+        };
+        return null;
     }
 
-	return {
-		startActivity: startActivity,
-		bleStartScan: bleStartScan,
-		bleStopScan: bleStopScan,
-        bleConnect: bleConnect,
-        bleDisconnect: bleDisconnect
-	};
-	
+    // ASCII only
+    function stringToBytes(string) {
+       var array = new Uint8Array(string.length);
+       for (var i = 0, l = string.length; i < l; i++) {
+           array[i] = string.charCodeAt(i);
+        }
+        return array.buffer;
+    }
+
+    // ASCII only
+    function bytesToString(buffer) {
+        return String.fromCharCode.apply(null, new Uint8Array(buffer));
+    }
+
+    return {
+        startActivity: startActivity,
+        ble: {
+            startScan: bleStartScan,
+            stopScan: bleStopScan,
+            connect: bleConnect,
+            disconnect: bleDisconnect,
+            write: bleWrite,
+            stringToBytes: stringToBytes,
+            bytesToString: bytesToString
+        }
+        
+    };
+    
 });
--- www_src/src/view/bleView.js
+++ www_src/src/view/bleView.js
@@ -32,13 +32,11 @@ define(["app", "plugins", "common/pubsub"], function(app, plugins, pubsub) {
         handleScan: function() {
             var that = this;
             if (that.state.isScan) { //扫描中
-                plugins.bleStopScan(function() {
+                plugins.ble.stopScan(function() {
                     that.setState({
                         scanLabel: "开始扫描",
                         isScan: false
                     });
-                }, function() {
-                    alert("扫描停止失败，请重试");
                 });
             } else { //未扫描
                 that.setState({
@@ -50,12 +48,10 @@ define(["app", "plugins", "common/pubsub"], function(app, plugins, pubsub) {
                 discoveredDevices = [];
                 pubsub.publish('scan', discoveredDevices);
 
-                plugins.bleStartScan([], function(device) {
+                plugins.ble.startScan([], function(device) {
                     //发现一个设备则回调一次
                     discoveredDevices.push(device);
                     pubsub.publish('scan', discoveredDevices);
-                }, function() {
-                    alert("扫描启动失败，请重试");
                 });
             }
         }
@@ -76,9 +72,6 @@ define(["app", "plugins", "common/pubsub"], function(app, plugins, pubsub) {
         componentWillUnmount: function() {
             pubsub.unsubscribe(this.pubsub_token);
         },
-        handleUploadData: function(i) {
-            alert(this.state.devices[i].id);
-        },
         render: function() {
             return (
                 <div className="list-block">
@@ -89,7 +82,7 @@ define(["app", "plugins", "common/pubsub"], function(app, plugins, pubsub) {
                               <div className="item-inner">
                                 <div className="item-title">{item.name}({item.id})</div>
                                 <div className="item-after">
-                                    <a href="#" className="button active" onClick={this.handleUploadData.bind(this, i)} key={i}>上传数据</a>
+                                    <UploadBtn seq={i}/>
                                 </div>
                               </div>
                             </li>
@@ -97,6 +90,62 @@ define(["app", "plugins", "common/pubsub"], function(app, plugins, pubsub) {
                         }, this)}
                     </ul>
                 </div>
+            );
+        },
+    });
+
+    var UploadBtn = React.createClass({
+        getInitialState: function() {
+            return {
+                status: 'unConnected',
+                label: '上传数据',
+                btnDisabled: false
+            };
+        },
+        handleConnect: function(i) {
+            var that = this;
+            that.setState({
+                status: "connecting",
+                label: '连接中...',
+                btnDisabled: true
+            });
+            plugins.ble.connect(discoveredDevices[i].id, function(peripheral){
+                that.setState({
+                    status: "connected",
+                    label: '连接成功'
+                });
+                console.log(JSON.stringify(peripheral));
+                that.handleSendData(peripheral);
+            });
+        },
+        handleDisconnect: function(deviceId) {
+            var that = this;
+            plugins.ble.disconnect(deviceId, function(){
+                that.setState({
+                    status: 'unConnected',
+                    label: '上传数据',
+                    btnDisabled: false
+                });
+            });
+        },
+        handleSendData: function(peripheral) {
+            var that = this;
+            var data = plugins.ble.stringToBytes("bbb");
+            plugins.ble.write(peripheral, data, function(){
+                alert("send ok!");
+                that.handleDisconnect(peripheral.id);
+            }, function(errorMsg){
+                // alert("指令发送失败:" + errorMsg);
+                that.handleDisconnect(peripheral.id);
+            });
+        },
+        render: function() {
+            var className = "button active";
+            if (this.state.btnDisabled) {
+                className = "button active disabled";
+            }
+            return (
+                <a href="#" className={className} onClick={this.handleConnect.bind(this, this.props.seq)}>{this.state.label}</a>
             );
         },
     });
